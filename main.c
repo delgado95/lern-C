ac4f63c78c7e335c544f8a7136f707476c67a7a7
João Vitor Delgado <82235790+delgado95@users.noreply.github.com>
2021-12-06 13:12:15 +0000

    sum of notes

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+  float nota1;
+  float nota2;
+  float soma;
+
+  printf("Este programa faz a soma de suas notas!\n");
+
+  printf("Insira aqui sua primeira nota: \n");
+  scanf("%f", &nota1);
+
+  printf("Insira aqui a segunda nota: \n");
+  scanf("%f", &nota2);
+
+  soma = nota1 + nota2;
+
+  printf("A soma das suas notas é = %f", soma);
+
   return 0;
 }
