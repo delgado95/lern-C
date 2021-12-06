0becd42c81be2164eb2fa39943aa21d5985ac9b0
João Vitor Delgado <82235790+delgado95@users.noreply.github.com>
2021-12-06 14:58:51 +0000

    func creation outside the main func

--- main.c
+++ main.c
@@ -1,27 +1,15 @@
 #include <stdio.h>
 
-// Declarando variaveis fora da função
-extern int a, b;
-extern int c;
-extern float f;
+// Declarando da função
+int func();
 
 int main() {
+  /*Chamando a função*/
+  int i = func();  
+}
 
-  /*Definição da variavel*/
-  int a, b;
-  int c;
-  float f;
-
-  /*Atribuição*/
-  a = 10;
-  b = 20;
-
-  c = a + b;
-  printf("O valor de c: %d \n", c);
-
-  f = 70.0/3.0;
-  printf("O valor de f: %f \n", f);
-
+/*Definição da função*/
+int func(){
   return 0;
 }
 
