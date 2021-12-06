ed53ad67abfa1049e15061e782cfbb498a7115c1
João Vitor Delgado <82235790+delgado95@users.noreply.github.com>
2021-12-06 21:17:47 +0000

    calculate area with const

--- main.c
+++ main.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
-#define LENGTH 10
-#define WIDTH 5
-#define NEWLINE '\n'
-
 int main() {
-  int area;
+   const int  LENGTH = 10;
+   const int  WIDTH = 5;
+   const char NEWLINE = '\n';
+   int area;  
+   
+   area = LENGTH * WIDTH;
+   printf("value of area : %d", area);
+   printf("%c", NEWLINE);
 
-  area = LENGTH * WIDTH;
-  printf("O valor da area : %d", area);
-  printf("%c", NEWLINE);
-  
-  return 0;
+   return 0;
 }
