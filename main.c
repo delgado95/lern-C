04d6c49294558cb621df75f2b1ca629ce91e3e95
João Vitor Delgado <82235790+delgado95@users.noreply.github.com>
2021-12-07 15:55:16 +0000

    testing static variables

--- main.c
+++ main.c
@@ -1,45 +1,22 @@
 #include <stdio.h>
 
-int main() {
-  float projetos[10];
-  int i;
-  struct{
-    int numero;
-    float valor;
-    char tipo;
-  }boleto;
+/*Declaração da função*/
+void func(void);
 
-  /* Inicia o vetor */
-  for (i=0; i < 10; i++){
-    projetos[i] = 0.0;
+static int count = 5; /*Variavel global*/
+
+main(){
+
+  while(count--){
+    func();
   }
+  return 0;
+}
 
-  printf("\n\nDigite o codigo do projeto: ");
-  scanf("%d", &boleto.numero);
+/*Definição da função*/
+void func(void){
 
-  while(boleto.numero != -1){
-    printf("\n\nDigite o valor: ");
-    scanf("%f", &boleto.valor);
+  static int i = 5; /*Varival estatica local*/
 
-    printf("\n\nDigite o tipo de transação (R ou D): ");
-    getchar();//Limpa o enter anterior
-    scanf("%c", &boleto.tipo);
-
-    if (boleto.tipo == 'R' || boleto.tipo == 'r'){
-      projetos[boleto.numero] = projetos[boleto.numero] + boleto.valor;
-    }
-    else{
-      if (boleto.tipo == 'D' || boleto.tipo == 'd'){
-        projetos[boleto.numero] = projetos[boleto.numero] + boleto.valor;
-      }
-      else{
-        printf("Tipo invalido !!");
-      }
-    }
-    printf("\n\nDigite o codigo do projeto: ");
-    scanf("%d", &boleto.numero);
-  }
-  for (i=0; i < 10; i++){
-    printf("\nSaldo do projeto %d = %f",i, projetos[i]);
-  }
+  printf("i é %d e count é %d \n", i, count);
 }
