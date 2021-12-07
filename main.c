be799d1a99ed9fb07298c8ab4495a7fa1c572895
João Vitor Delgado <82235790+delgado95@users.noreply.github.com>
2021-12-07 16:51:46 +0000

    are treated as ​​local variables in a function and take precedence over ​​global variables

--- main.c
+++ main.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
  
 /* global variable declaration */
-int g = 20;
+int a = 20;
  
 int main () {
 
-  /* local variable declaration */
-  int g = 10;
- 
-  printf ("value of g = %d\n",  g);
- 
+  /* local variable declaration in main function */
+  int a = 10;
+  int b = 20;
+  int c = 0;
+
+  printf ("value of a in main() = %d\n",  a);
+  c = sum( a, b);
+  printf ("value of c in main() = %d\n",  c);
+
   return 0;
+}
+
+/* function to add two integers */
+int sum(int a, int b) {
+
+   printf ("value of a in sum() = %d\n",  a);
+   printf ("value of b in sum() = %d\n",  b);
+
+   return a + b;
 }
