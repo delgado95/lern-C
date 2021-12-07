8a143f76a1019d5d8bfb173dcfe2b139172b435e
João Vitor Delgado <82235790+delgado95@users.noreply.github.com>
2021-12-07 17:45:20 +0000

    pointer returns null

--- main.c
+++ main.c
@@ -2,18 +2,9 @@
 
 int main () {
 
-   int  var = 20;   /* actual variable declaration */
-   int  *ip;        /* pointer variable declaration */
+   int  *ptr = NULL;
 
-   ip = &var;  /* store address of var in pointer variable*/
-
-   printf("Address of var variable: %x\n", &var  );
-
-   /* address stored in pointer variable */
-   printf("Address stored in ip variable: %x\n", ip );
-
-   /* access the value using the pointer */
-   printf("Value of *ip variable: %d\n", *ip );
-
+   printf("The value of ptr is : %x\n", ptr  );
+ 
    return 0;
 }
