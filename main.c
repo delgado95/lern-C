4270cb54279e142dd440c94e33fe9b9317b33dbf
João Vitor Delgado <82235790+delgado95@users.noreply.github.com>
2021-12-07 16:00:10 +0000

    exporting variables

--- main.c
+++ main.c
@@ -1,22 +1,9 @@
 #include <stdio.h>
 
-/*Declaração da função*/
-void func(void);
-
-static int count = 5; /*Variavel global*/
+int count;
+extern void write_extern();
 
 main(){
-
-  while(count--){
-    func();
-  }
-  return 0;
-}
-
-/*Definição da função*/
-void func(void){
-
-  static int i = 5; /*Varival estatica local*/
-
-  printf("i é %d e count é %d \n", i, count);
+  count = 5;
+  write_extern();
 }
