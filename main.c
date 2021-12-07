389ac85a7a4ba5e935f02c8dca7c3eec3feecc57
João Vitor Delgado <82235790+delgado95@users.noreply.github.com>
2021-12-07 17:38:26 +0000

    taking a variable in the processor

--- main.c
+++ main.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
- 
+
 int main () {
 
-   int n[ 10 ]; /* n is an array of 10 integers */
-   int i,j;
- 
-   /* initialize elements of array n to 0 */         
-   for ( i = 0; i < 10; i++ ) {
-      n[ i ] = i + 100; /* set element at location i to i + 100 */
-   }
-   
-   /* output each array element's value */
-   for (j = 0; j < 10; j++ ) {
-      printf("Element[%d] = %d\n", j, n[j] );
-   }
- 
+   int  var1;
+   char var2[10];
+
+   printf("Address of var1 variable: %x\n", &var1  );
+   printf("Address of var2 variable: %x\n", &var2  );
+
    return 0;
 }
+
+//usando o operador E comercial (&), que denota um endereço na memória
