f2a8ef3c73e016a84cc418c2173f7170662ff48c
João Vitor Delgado <82235790+delgado95@users.noreply.github.com>
2021-12-07 17:35:44 +0000

    creating an array

--- main.c
+++ main.c
@@ -1,25 +1,19 @@
-#include<stdio.h>
-#include<string.h>
-int main(){
- char frase[101];
- char letra;
- int i, cont, tam ;
+#include <stdio.h>
+ 
+int main () {
 
- printf("\n\nDigite uma frase (Max. 100 caracteres): ");
- gets(frase);
- tam = strlen(frase);
- printf("\n\nDigite a letra que você deseja procurar: ");
- scanf("%c",&letra);
-
- cont =0;
- i=0;
- while (i <= tam) {
-
-   if (frase[i] == letra) {
-     cont = cont + 1;
+   int n[ 10 ]; /* n is an array of 10 integers */
+   int i,j;
+ 
+   /* initialize elements of array n to 0 */         
+   for ( i = 0; i < 10; i++ ) {
+      n[ i ] = i + 100; /* set element at location i to i + 100 */
    }
-   i++;
- }
- printf("\n\nA letra \'%c\' apareceu %d vezes", letra, cont);
-
+   
+   /* output each array element's value */
+   for (j = 0; j < 10; j++ ) {
+      printf("Element[%d] = %d\n", j, n[j] );
+   }
+ 
+   return 0;
 }
