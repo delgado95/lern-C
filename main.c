06ab87c5e1db31eb8a9ae5e44cdc6901597d47c0
João Vitor Delgado <82235790+delgado95@users.noreply.github.com>
2021-12-07 16:43:44 +0000

    sums with local variables

--- main.c
+++ main.c
@@ -1,32 +1,17 @@
 #include <stdio.h>
+ 
+int main () {
 
-/*Declaração da função*/
-int max(int num1, int num2);
-
-int main(){
-
-  /*Declaração da variavel*/
-  int a = 100;
-  int b = 200;
-  int ret;
-
-  /*Chamando a função*/
-  ret = max(a, b);
-
-  printf("Max value is: %d\n", ret);
-
+  /* local variable declaration */
+  int a, b;
+  int c;
+ 
+  /* actual initialization */
+  a = 10;
+  b = 20;
+  c = a + b;
+ 
+  printf ("value of a = %d, b = %d and c = %d\n", a, b, c);
+ 
   return 0;
-}
-
-/*Função retornando o max entre dois numeros*/
-int max(int num1, int num2){
-
-  /*Declaração da variavel*/
-  int result;
-
-  if(num1 > num2)
-    result = num1;
-  else
-    result = num2;
-  return result;
 }
