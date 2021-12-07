8ee7ada5364188ab0dc325edf4713ca572e5c2a4
João Vitor Delgado <82235790+delgado95@users.noreply.github.com>
2021-12-07 16:45:34 +0000

    sums with global variable

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
  
+/* global variable declaration */
+int g;
+ 
 int main () {
 
   /* local variable declaration */
   int a, b;
-  int c;
  
   /* actual initialization */
   a = 10;
   b = 20;
-  c = a + b;
+  g = a + b;
  
-  printf ("value of a = %d, b = %d and c = %d\n", a, b, c);
+  printf ("value of a = %d, b = %d and g = %d\n", a, b, g);
  
   return 0;
 }
