a5437260c6bbc9c8dc573ab6614a2a9d9850c39c
João Vitor Delgado <82235790+delgado95@users.noreply.github.com>
2021-12-10 20:31:04 +0000

    using poniter with struct

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ struct Books {
 };
 
 /*Declarando a função de mostra em tela*/
-void printbook( struct Books book);
+void printbook( struct Books *book);
 
 int main () {
 
@@ -28,20 +28,20 @@ int main () {
   strcpy( Book2.subject, "O amigo da vizinhaça !");
   Book2.book_id = 2;
   
-  /*Mostrando as informações do livro 1*/
+  /*Mostrando o livro 1 passando o endereço*/
 
-  printbook(Book1);
+  printbook(&Book1);
 
-  /*Mostrando as informações do livro 2*/
-  printbook(Book2);
+  /*Mostrando o livro 2 passando o endereço*/
+  printbook(&Book2);
 
   return 0;
 }
 
-void printbook( struct Books book){
+void printbook( struct Books *book){
  /*Mostrando as informações do livro 1*/
-  printf("Book title    : %s\n", book.title);
-  printf("Book author   : %s\n", book.author);
-  printf("Book subject  : %s\n", book.subject);
-  printf("Book id       : %d\n", book.book_id);
+  printf("Book title    : %s\n", book->title);
+  printf("Book author   : %s\n", book->author);
+  printf("Book subject  : %s\n", book->subject);
+  printf("Book id       : %d\n", book->book_id);
 }
