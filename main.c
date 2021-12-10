72dbe83b47355c766e3fa48b860196fcb708c988
João Vitor Delgado <82235790+delgado95@users.noreply.github.com>
2021-12-10 20:14:34 +0000

    using struct

--- main.c
+++ main.c
@@ -1,24 +1,41 @@
 #include <stdio.h>
 #include <string.h>
 
+struct Books {
+  char title[50];
+  char author[50];
+  char subject[100];
+  int book_id;
+};
+
 int main () {
 
-   char str1[12] = "Hello";
-   char str2[12] = "World";
-   char str3[12];
-   int  len ;
+  struct Books Book1; /*Declaração dos tipos de book*/
+  struct Books Book2;
 
-   /* copy str1 into str3 */
-   strcpy(str3, str1);
-   printf("strcpy( str3, str1) :  %s\n", str3 );
+  /*Especificação do livro 1*/
+  strcpy( Book1.title, "Star Wars");
+  strcpy( Book1.author, "Jorge Lucas");
+  strcpy( Book1.subject, "Uma viagem muito louca !");
+  Book1.book_id = 1;
+  
+  /*Especificação do livro 2*/
+  strcpy( Book2.title, "Homem Aranha");
+  strcpy( Book2.author, "Stan Lee");
+  strcpy( Book2.subject, "O amigo da vizinhaça !");
+  Book2.book_id = 2;
+  
+  /*Mostrando as informações do livro 1*/
+  printf("Book 1 title    : %s\n", Book1.title);
+  printf("Book 1 author   : %s\n", Book1.author);
+  printf("Book 1 subject  : %s\n", Book1.subject);
+  printf("Book 1 id       : %d\n", Book1.book_id);
 
-   /* concatenates str1 and str2 */
-   strcat( str1, str2);
-   printf("strcat( str1, str2) :  %s\n", str1 );
+  /*Mostrando as informações do livro 1*/
+  printf("Book 2 title    : %s\n", Book2.title);
+  printf("Book 2 author   : %s\n", Book2.author);
+  printf("Book 2 subject  : %s\n", Book2.subject);
+  printf("Book 2 id       : %d\n", Book2.book_id);
 
-   /* total lenghth of str1 after concatenation */
-   len = strlen(str1);
-   printf("strlen(str1) :  %d\n", len );
-
-   return 0;
+  return 0;
 }
