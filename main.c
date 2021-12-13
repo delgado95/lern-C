1309c0a9c5d06c99b28bbf88c904d36412e2e806
João Vitor Delgado <82235790+delgado95@users.noreply.github.com>
2021-12-13 16:08:22 +0000

    the variables defined with a predefined width are called bit fields

--- main.c
+++ main.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 
-/* define simple structure */
 struct {
-   unsigned int widthValidated;
-   unsigned int heightValidated;
-} status1;
+   unsigned int age : 3;
+} Age;
 
-/* define a structure with bit fields */
-struct {
-   unsigned int widthValidated : 1;
-   unsigned int heightValidated : 1;
-} status2;
- 
 int main( ) {
-   printf( "Memory size occupied by status1 : %d\n", sizeof(status1));
-   printf( "Memory size occupied by status2 : %d\n", sizeof(status2));
+
+   Age.age = 4;
+   printf( "Sizeof( Age ) : %d\n", sizeof(Age) );
+   printf( "Age.age : %d\n", Age.age );
+
+   Age.age = 7;
+   printf( "Age.age : %d\n", Age.age );
+
+   Age.age = 8;
+   printf( "Age.age : %d\n", Age.age );
+
    return 0;
 }
