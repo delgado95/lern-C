879ecd561d08b75f8066076aa24e20358388f2fa
João Vitor Delgado <82235790+delgado95@users.noreply.github.com>
2021-12-13 17:29:45 +0000

    it permits two separate tokens in the macro definition to be joined into a single token

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-#define  message_for(a, b)  \
-   printf(#a " and " #b ": We love you!\n")
+#define tokenpaster(n) printf ("token" #n " = %d", token##n)
 
 int main(void) {
-   message_for(Carole, Debra);
+   int token34 = 40;
+   tokenpaster(34);
    return 0;
 }
