aaa164b161360a3f19fe047bbff1e1e02e768852
João Vitor Delgado <82235790+delgado95@users.noreply.github.com>
2021-12-13 17:17:12 +0000

    update

--- main.c
+++ main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
+ 
+#define TRUE  1
+#define FALSE 0
+ 
 int main( ) {
-
-   int c;
-
-   printf( "Enter a value :");
-   c = getchar( );
-
-   printf( "\nYou entered: ");
-   putchar( c );
+   printf( "Value of TRUE : %d\n", TRUE);
+   printf( "Value of FALSE : %d\n", FALSE);
 
    return 0;
 }
