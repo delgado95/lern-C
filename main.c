cdff1e9349d940291b85d571567f973dc0530181
João Vitor Delgado <82235790+delgado95@users.noreply.github.com>
2021-12-13 17:18:08 +0000

    function puts the passed character on the screen and returns the same character

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
- 
-#define TRUE  1
-#define FALSE 0
- 
 int main( ) {
-   printf( "Value of TRUE : %d\n", TRUE);
-   printf( "Value of FALSE : %d\n", FALSE);
+
+   int c;
+
+   printf( "Enter a value :");
+   c = getchar( );
+
+   printf( "\nYou entered: ");
+   putchar( c );
 
    return 0;
 }
