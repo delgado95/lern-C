3f2458f95036338e76bad1880b57a4d59761e8c2
João Vitor Delgado <82235790+delgado95@users.noreply.github.com>
2021-12-14 11:34:29 +0000

    solve many mathematical problems

--- main.c
+++ main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
-unsigned long long int factorial(unsigned int i) {
+int fibonacci(int i) {
 
-   if(i <= 1) {
+   if(i == 0) {
+      return 0;
+   }
+	
+   if(i == 1) {
       return 1;
    }
-   return i * factorial(i - 1);
+   return fibonacci(i-1) + fibonacci(i-2);
 }
 
 int  main() {
-   int i = 12;
-   printf("Factorial of %d is %d\n", i, factorial(i));
+
+   int i;
+	
+   for (i = 0; i < 10; i++) {
+      printf("%d\t\n", fibonacci(i));
+   }
+	
    return 0;
 }
