201b8aa9154d4e8c968b4ddf3c8bc40bed6c45b6
João Vitor Delgado <82235790+delgado95@users.noreply.github.com>
2021-12-14 11:37:05 +0000

    allocating memory

--- main.c
+++ main.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
-#include <stdarg.h>
-
-double average(int num,...) {
-
-   va_list valist;
-   double sum = 0.0;
-   int i;
-
-   /* initialize valist for num number of arguments */
-   va_start(valist, num);
-
-   /* access all the arguments assigned to valist */
-   for (i = 0; i < num; i++) {
-      sum += va_arg(valist, int);
-   }
-	
-   /* clean memory reserved for valist */
-   va_end(valist);
-
-   return sum/num;
-}
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-   printf("Average of 2, 3, 4, 5 = %f\n", average(4, 2,3,4,5));
-   printf("Average of 5, 10, 15 = %f\n", average(3, 5,10,15));
+
+   char name[100];
+   char *description;
+
+   strcpy(name, "Zara Ali");
+
+   /* allocate memory dynamically */
+   description = malloc( 200 * sizeof(char) );
+	
+   if( description == NULL ) {
+      fprintf(stderr, "Error - unable to allocate required memory\n");
+   } else {
+      strcpy( description, "Zara ali a DPS student in class 10th");
+   }
+   
+   printf("Name = %s\n", name );
+   printf("Description: %s\n", description );
 }
