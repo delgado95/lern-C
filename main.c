2f7cb0c11e5cd96d1d1f2de62c985d078f87ef9a
João Vitor Delgado <82235790+delgado95@users.noreply.github.com>
2021-12-18 04:42:25 +0000

    after swapping

--- main.c
+++ main.c
@@ -1,19 +1,22 @@
-#include <stdio.h>
+#include<stdio.h>
+int main() {
+  double first, second, temp;
+  printf("Enter first number: ");
+  scanf("%lf", &first);
+  printf("Enter second number: ");
+  scanf("%lf", &second);
 
-int main (){
+  // value of first is assigned to temp
+  temp = first;
 
-  int num1;
+  // value of second is assigned to first
+  first = second;
 
-  printf("Vamos ver se o numero é par ou impar !\n");
+  // value of temp (initial value of first) is assigned to second
+  second = temp;
 
-  printf("Insira qualquer nuemro inteiro: \n");
-  scanf("%d", &num1);
-
-  if (num1%2 == 0)
-    printf("é par \n");
-  else 
-    printf("é impar \n");
-
+  // %.2lf displays number up to 2 decimal points
+  printf("\nAfter swapping, first number = %.2lf\n", first);
+  printf("After swapping, second number = %.2lf", second);
   return 0;
-  
 }
